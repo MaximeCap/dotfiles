6e1ed8a45b18722165edede18b177bdd84831bbc
maxime.cappellen <maxime.cappellen@live.fr>
2024-08-21 14:40:12 +0200

    feat: add loading in install script

--- install-script/install.sh
+++ install-script/install.sh
@@ -1,28 +1,41 @@
+source ~/dotfiles/utils/spinner.sh
 #==============
 # Remove previous old files
 #==============
 
-sudo rm -rf ~/.vim > /dev/null 2>&1
-sudo rm -rf ~/.zshrc > /dev/null 2>&1
-sudo rm -rf ~/.config > /dev/null 2>&1
-sudo rm -rf ~/.p10k.zsh > /dev/null 2>&1
+
+sudo rm -rf ~/.vim > /dev/null 2>&1 
+sudo rm -rf ~/.zshrc > /dev/null 2>&1 
+sudo rm -rf ~/.config > /dev/null 2>&1 
+sudo rm -rf ~/.p10k.zsh > /dev/null 2>&1 
+sudo rm -rf ~/.local/state/nvim ~/.local/share/nvim > /dev/null 2>&1 
 
 #==============
 # Install all the packages
 #==============
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Installing Brew ..."
 
-brew doctor
-brew update
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &
+brew doctor &
+brew update &
+spin $!
 
-brew bundle --file=~/dotfiles/brew/Brewfile
-
-# Clone plugins
-git clone https://github.com/tmux-plugins/tpm ~/dotfiles/tmux/plugins/tpm
-cd ~/dotfiles
-
-source ~/dotfiles/.config/tmux/tmux.conf
-./../.config/tmux/plugins/tpm/scripts/install_plugins.sh
+echo "Brewing ..."
+brew bundle --file=~/dotfiles/brew/Brewfile &
+spin $!
 
 # Init stow symlinks
 stow .
+
+# Clone plugins
+tmpDir="~/dotfiles/.config/tmux/plugins/tpm"
+if [ ! -d "$dir"]; then
+  echo "No TPM Plugins found; Cloning them"
+  git clone https://github.com/tmux-plugins/tpm ~/dotfiles/.config/tmux/plugins/tpm
+fi
+
+tmux source ~/dotfiles/.config/tmux/tmux.conf
+
+cd ~/dotfiles
+./.config/tmux/plugins/tpm/scripts/install_plugins.sh
+
