29adb5f8877a57167b8fc278205278ec7f3f2d61
Maxime Cappellen <maxime.cappellen@intech.lu>
2024-08-26 07:45:29 +0200

    feat: update linter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -699,11 +699,11 @@ require("lazy").setup({
       local lint = require("lint")
 
       lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        svelte = { "eslint_d" },
+        javascript = { "eslint" },
+        typescript = { "eslint" },
+        javascriptreact = { "eslint" },
+        typescriptreact = { "eslint" },
+        svelte = { "eslint" },
         python = { "pylint" },
         markdown = { "markdownlint" },
       }
@@ -766,10 +766,10 @@ require("lazy").setup({
           }
         end,
         formatters_by_ft = {
-          javascript = { "eslint_d" },
-          typescript = { "eslint_d" },
-          javascriptreact = { "eslint_d" },
-          typescriptreact = { "eslint_d" },
+          javascript = { "eslint" },
+          typescript = { "eslint" },
+          javascriptreact = { "eslint" },
+          typescriptreact = { "eslint" },
           svelte = { "prettier" },
           css = { "prettier" },
           html = { "prettier" },
