42263729f316e7a47492a2a7b8f4080d745b2cd0
Maxime Cappellen <maxime.cappellen.e@thalesdigital.io>
2024-09-10 14:24:52 +0200

    remove blur

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,8 +8,8 @@ config.font_size = 13
 config.window_decorations = "RESIZE"
 config.color_scheme = "Batman"
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 10
 
 config.colors = {
 	foreground = "#CBE0F0",
